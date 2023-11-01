f01667bac4e73ef4c3867114ade45629ccaa4b34
lc2101 <lrcroci@gmail.com>
2023-11-01 15:08:16 -0300

    home/landing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',
+  component:LandingComponent,
+  loadChildren: () => import("./landing/landing.module").then(n=>n.LandingModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/landing/landing/landing.component.ts
@@ -0,0 +1,11 @@
+import { Component, NgModule } from '@angular/core';
+
+@Component({
+  selector: 'app-landing',
+  templateUrl: './landing.component.html',
+  styleUrls: ['./landing.component.css']
+})
+
+export class LandingComponent {
+
+}
